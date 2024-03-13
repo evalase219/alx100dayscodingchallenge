564375827aa3a1ba1ef355577cb9ad0ec3f8e0ea
evalase219 <chiomaevangeline85@gmail.com>
2024-03-13 23:07:28 +0100

    Loop right angle triangle

---
+++ 0x01_day/0_loop-triangle.js
@@ -0,0 +1,11 @@
+function loopRightTriangle(n) {
+  result = "";
+  for (let row = 1; row <= n; row++) {
+    //print asterisks for number of times it runs through the row
+    for (let col = 1; col <= row; col++) {
+      result += "*";
+    }
+    result += "\n";
+  }
+  console.log(result);
+}
---
+++ 0x01_day/README.md
@@ -0,0 +1,2 @@
+Today which is day 1, i practise how to loop through a
+triangle pattern and print it.
